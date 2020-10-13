2cf4c63d626c0d041b58f0f97d3b9942493fcec1
Daniel Franca <daniel.pe.15y15@gmail.com>
2020-10-12 22:49:29 -0300

    add lint script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Oi mundo</h1>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Oi mundo</h1>
+  </div>
+);
 
 export default App;
